ea0f72cace43b03e901bf62ded342693ac2a6481
Allyson Wesman <allysonwesman@gmail.com>
2017-12-24 06:14:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {foo: 'bar'};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName: songTitle];
+  playlist[artistName] = songTitle;
   return playlist;
 }
